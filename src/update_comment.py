bf352cb5f9eaf3049682fc5da2779d4fb4754fd8
weichen <a4793706@gmail.com>
2020-08-19 01:10:09 +0800

    fix update_comment style and format

--- src/update_comment.py
+++ src/update_comment.py
@@ -3,28 +3,20 @@ import argparse
 import pandas as pd
 import json
 from time import gmtime, strftime
-from os.path import join, abspath, dirname
-from requests.exceptions import HTTPError
-from collections import defaultdict
-from .api import OwnerApi
-from .config import Config
-from .youtube.channel_api import ChannelApi
-from .youtube.youtube_api import YoutubeApi
-from .md import Mongodb
+from os import path
 from itertools import chain
+from eyescomment.api import OwnerApi
+from eyescomment.config import Config
+from eyescomment.youtube.channel_api import ChannelApi
+from eyescomment.youtube_api import YoutubeApi
+from eyescomment.md import Mongodb
 
 
 logger = logging.getLogger(__name__)
-CURRENT_PATH = dirname(abspath(__file__))
-TRAIN_DIR = join(CURRENT_PATH, 'eyesComment/data/unlabel')
-FEATURESLABEL = 'feature.json'
-APIKEY = [
-    'AIzaSyDDa9SL4Rk4oVGj6rHHqzmZmJSIewGCUgg',
-    'AIzaSyCGokxpLFG-7M259tOp7-q7fsqYKqvmQNE',
-    'AIzaSyD08pO1kEyZ1t7RXQuAyUFlOTyJO68FZYg',
-    'AIzaSyBOWzgpes4ryDn0BHthJjj7vcGr1VlpndA',
-    'AIzaSyBaFMdTVrz6pJhSosmWNMaailKVWElkjIw'
-]
+CURRENT_PATH = path.dirname(path.abspath(__file__))
+TRAIN_DIR = path.join(
+    CURRENT_PATH, 'smart_feautres/eyesComment/data/unlabel')
+FEATURES_LABEL = 'feature.json'
 
 
 def _parse_args():
@@ -39,7 +31,8 @@ def _parse_args():
                         help='youtube video unique id')
     parser.add_argument('--channel-id', nargs='+', default=None,
                         help='youtube channel')
-    parser.add_argument('--youtube-api-key', default='AIzaSyBKWCDhu4PumaIgwie_hHw602uOHFWgR1o',
+    parser.add_argument('--youtube-api-key',
+                        default='AIzaSyBKWCDhu4PumaIgwie_hHw602uOHFWgR1o',
                         help='youtube api key')
     parser.add_argument('--dry-run', action='store_true',
                         help='Show results only, do not plubic')
@@ -51,21 +44,24 @@ def _parse_args():
 class VideoApi(OwnerApi):
     def __init__(self, path):
         super(VideoApi, self).__init__(
-            host=Config(join(CURRENT_PATH, 'config.json')).content['PORTAL_SERVER'], path=path
+            host=Config.instance().get('PORTAL_SERVER'),
+            path=path
         )
-    
+
     def get(self, params=None):
         data = super(VideoApi, self).get(params={"filter": json.dumps(params)})
         return data
 
+
 class AbstractFeatures():
-    featuresLabel_dir = join(CURRENT_PATH, 'featuresLabel', FEATURESLABEL)
+    features_label_dir = path.join(
+        CURRENT_PATH, 'features_Label', FEATURES_LABEL)
     frontKeys = []
     filterKeys = ['publishedAt', 'updatedAt'] + frontKeys
 
     @property
     def featuresDict(self):
-        with open(self.featuresLabel_dir, 'r') as js:
+        with open(self.features_label_dir, 'r') as js:
             f = json.load(js)
         return f
 
@@ -79,7 +75,7 @@ class AbstractFeatures():
         return series
 
     def trans_seriesFeatures(self, currentSeries, featuresDict):
-        """ transfer current series key to key of json file 
+        """ transfer current series key to key of json file
         Args:
             currentSeries: the current series
             featuresDict: json file with new feature
@@ -108,35 +104,42 @@ class AbstractFeatures():
                 for value in data_values:
                     # logging.info('saving {} policy_name dataframe to csv'.format(ser['video_id']))
                     transSer = self.trans_seriesFeatures(value, featuresDict)
-                    newDf = pd.DataFrame(transSer, columns = featuresColumn, index=[0])
+                    newDf = pd.DataFrame(transSer, columns=featuresColumn, index=[0])
                     df = pd.concat([df, newDf], ignore_index=True)
         return df
 
-    def save(self, fileDIR, series):
+    def save(self, file_dir, series):
         df = self.gen_trainingData(series)
-        FILE_DIR = gen_filePath(fileDIR)
-        df.fillna(0).to_csv(FILE_DIR, index=False, encoding='utf-8-sig')   
+        file_path = gen_file_path(file_dir)
+        df.fillna(0).to_csv(file_path, index=False, encoding='utf-8-sig')
 
 
-def gen_filePath(fileDir):
+def gen_file_path(file_dir):
     date = strftime("%Y-%m-%d-%H-%M", gmtime())
-    fileName = "{}_.csv".format(date)
-    filePath = join(fileDir, fileName)
-    return filePath
+    file_name = "{}_.csv".format(date)
+    return path.join(file_dir, file_name)
 
 
 def get_video_id(channel_id):
-    channelApi = ChannelApi(Config(join(CURRENT_PATH, 'config.json')).content['PORTAL_SERVER'], 'Youtube_videos')
-    for channel_detail in channelApi.get(params={"where":{"channelId": channel_id}}):
+    channel_api = ChannelApi(
+        host=Config.instance().get('PORTAL_SERVER'),
+        target_path='Youtube_videos',
+        cache_path=Config.instance().get('CACHE_DIR'))
+    channels_detail = channel_api.get(params={"where": {"channelId": channel_id}})
+    for channel_detail in channels_detail:
         yield channel_detail['videoId']
 
 
 def get_channel_id():
-    channelApi = ChannelApi(Config(join(CURRENT_PATH, 'config.json')).content['PORTAL_SERVER'], 'Youtube_videos')
-    video_ids = channelApi.get(params={"fields": {"channelId": True}})
+    channel_api = ChannelApi(
+        host=Config.instance().get('PORTAL_SERVER'),
+        target_path='Youtube_videos',
+        cache_path=Config.instance().get('CACHE_DIR'))
+    video_ids = channel_api.get(params={"fields": {"channelId": True}})
     ids = [video_id['channelId'] for video_id in video_ids]
     logger.info('loading channelId id from db')
     return ids
+
 
 class YoutubeApiHandler(YoutubeApi):
     def __init__(self, key):
@@ -145,11 +148,13 @@ class YoutubeApiHandler(YoutubeApi):
 
     def get_comment_detail(self, channel_id):
         for video_id in get_video_id(channel_id):
+            print('video:', video_id)
             yield video_id, self.gen_comment(video_id, 50)
-    
+
     def get_videos_comment(self, channels_id):
         for channel_id in channels_id:
             yield channel_id, dict(self.get_comment_detail(channel_id))
+
 
 class Handler(Mongodb):
     def __init__(
@@ -171,11 +176,13 @@ class Handler(Mongodb):
             if post_message:
                 self.insert_many(post_message)
                 logger.info(
-                    'pushing key: {} video comment detail to mongodb: {}'.format(video_id, video_comments_detail)
+                    'pushing key: {} video comment detail to mongodb: {}'.format(
+                        video_id, video_comments_detail)
                 )
             else:
                 logger.debug(
-                    'fial pushing key:{} video comment detail to mongodb: {}'.format(video_id, video_comments_detail)
+                    'fial pushing key:{} video comment detail to mongodb: {}'.format(
+                        video_id, video_comments_detail)
                 )
 
     def push_collection(self, comments_detail):
@@ -187,8 +194,10 @@ class Handler(Mongodb):
             )
             self.push_comment_detail(channel_comments_detail)
 
+
 def main():
     args = _parse_args()
+    Config.set_dir(path.join(CURRENT_PATH, 'config.json'))
     if not args.channel_id:
         args.channel_id = get_channel_id()
     YoutubeApihandler = YoutubeApiHandler(args.youtube_api_key)
@@ -201,6 +210,7 @@ def main():
             db=args.db,
             collection=args.collection
         ).push_collection(comments_detail)
+
 
 if __name__ == '__main__':
     logging.basicConfig(
