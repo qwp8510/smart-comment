9da10662699a80c618612392c7e5f2a572725786
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-01 16:26:36 +0800

    add smart_features static method model for getting model

---
+++ src/smart_features/models.py
@@ -0,0 +1,23 @@
+from .eyesComment import train as eyescomment_train
+from .eyesComment.detector import SentimentDetector as eyescomment_detector
+from .text_clustering import train as text_clustering_train
+
+
+EYESCOMMENT_BEHAVIOR = {'train': eyescomment_train, 'predict': eyescomment_detector}
+TEXT_CLUSTERING_BEHAVIOR = {'train': text_clustering_train}
+MODEL_TYPE = {'eyesComment': EYESCOMMENT_BEHAVIOR, 'text_clustering': TEXT_CLUSTERING_BEHAVIOR}
+
+
+class SmartFeatures():
+    @staticmethod
+    def model(model_type, model_behavior):
+        """ SmartFeatures model
+        Args:
+            model_type(str): choose model(eyesComment, text_clustering...)
+            model_behavior(str): choose model behavior(train, predict...)
+
+        Returns:
+            Model Instance
+
+        """
+        return MODEL_TYPE.get(model_type, {}).get(model_behavior)
