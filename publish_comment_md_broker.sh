1543c29dff708c3fcc5a309609b381fb6890f154
weichen-server <a4793706@gmail.com>
2021-01-21 20:39:49 +0800

    fix echo message

--- publish_comment_md_broker.sh
+++ publish_comment_md_broker.sh
@@ -4,7 +4,7 @@ src=`ps aux | grep [p]ublish_comment_md.py`
 
 if [ -n "$src" ]
 then
-  echo "publish_comment_md.py.py is still running"
+  echo "publish_comment_md.py is still running"
 else
   echo "running publish_comment_md.py"
   $PROG_PATH/.virtualenvs/smart-comment/bin/python3.6 $PROG_PATH/smart-comment/src/publish_comment_md.py
